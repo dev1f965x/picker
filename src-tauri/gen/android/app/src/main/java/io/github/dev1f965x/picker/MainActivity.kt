83e8440ea410bbaa4e0989635e95351a4daaef97
dev1f965x <315394198+dev1f965x@users.noreply.github.com>
2026-09-11 21:33:53 +0900

    ✨ feat: add the Tauri Android project
    Generated by tauri android init. Release builds are signed with the debug key,
    since the app is installed on personal devices only.

---
+++ src-tauri/gen/android/app/src/main/java/io/github/dev1f965x/picker/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.github.dev1f965x.picker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
